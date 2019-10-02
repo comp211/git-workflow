75af0def0efc7254a56a25e5ba1d786316b653d7
Kris Jordan <kris@cs.unc.edu>
2019-10-02 03:38:51 +0000

    Make a simple number guessing game.

--- main.c
+++ main.c
@@ -1,9 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
 
 int main()
 {
-    printf("Welcome to my awesome application...\n");
-    printf("Don't you think it's great?\n");
+    // Seed pseudo-random number generator
+    srand(time(0) + getpid());
+
+    int secret = rand() % 256;
+
+    printf("I'm thinking of a number between 0 and 255...\n");
+    int guess = -1;
+
+    do {
+        printf("What is it? ");
+        scanf("%d", &guess);
+        if (guess > secret) {
+            printf("Lower\n");
+        } else if (guess < secret) {
+            printf("Higher\n");
+        }
+    } while(guess != secret);
+
+    printf("Correct! Game over.\n");
+
     return EXIT_SUCCESS;
 }
